175e4b3e0ead92ef1b9d36b244403a8015b05991
Troy Landers <tlanders@landersconsulting.biz>
2021-01-29 16:57:42 -0600

    added top nav with custom animation effects

--- project3/script.js
+++ project3/script.js
@@ -1,4 +1,18 @@
+$(document).ready(function () {
+    $('.mynavbar-button').click(function () {
+        $('.mynavbar-button').toggleClass('navclick');
+    });
 
+    $(window).scroll(function () {
+        let position = $(this).scrollTop();
+
+        if(position > 200) {
+            $('.nav-menu').addClass('mynavbar-reduced');
+        } else {
+            $('.nav-menu').removeClass('mynavbar-reduced');
+        }
+    });
+});
 
 
 
